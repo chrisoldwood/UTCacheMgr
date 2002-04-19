af27b2fd3ca7b259a79c0ecc23263ba64a1b0f8f
Administrator <administrator@chrisoldwood.com>
2002-04-19 10:42:23 +0000

    Removed paths from default settings.

--- Profile.cpp
+++ Profile.cpp
@@ -19,14 +19,14 @@
 
 const char* CProfile::DEF_PROFILE_NAME   = "Default";
 const char* CProfile::DEF_ROOT_DIR       = "C:\\UnrealTournament";
-const char* CProfile::DEF_CACHE_DIR      = "C:\\UnrealTournament\\Cache";
+const char* CProfile::DEF_CACHE_DIR      = "Cache";
 const char* CProfile::DEF_CACHE_IDX_FILE = "cache.ini";
 const char* CProfile::DEF_CACHE_FILE_EXT = "uxx";
-const char* CProfile::DEF_SYSTEM_DIR     = "C:\\UnrealTournament\\System";
-const char* CProfile::DEF_MAPS_DIR       = "C:\\UnrealTournament\\Maps";
-const char* CProfile::DEF_TEXTURES_DIR   = "C:\\UnrealTournament\\Textures";
-const char* CProfile::DEF_SOUNDS_DIR     = "C:\\UnrealTournament\\Sounds";
-const char* CProfile::DEF_MUSIC_DIR      = "C:\\UnrealTournament\\Music";
+const char* CProfile::DEF_SYSTEM_DIR     = "System";
+const char* CProfile::DEF_MAPS_DIR       = "Maps";
+const char* CProfile::DEF_TEXTURES_DIR   = "Textures";
+const char* CProfile::DEF_SOUNDS_DIR     = "Sounds";
+const char* CProfile::DEF_MUSIC_DIR      = "Music";
 const char* CProfile::DEF_CONFIG_FILE    = "UnrealTournament.ini";
 
 /******************************************************************************
