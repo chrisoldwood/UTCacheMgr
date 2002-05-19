e71bf5a258386bd83bc7b8a70691f12da76f6346
Administrator <administrator@chrisoldwood.com>
2002-05-19 21:29:19 +0000

    Vss2Git

---
+++ HelpFile/HelpTopics.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+** (C) Chris Oldwood
+**
+** MODULE:		HELPTOPICS.H
+** COMPONENT:	The Application.
+** DESCRIPTION:	Help topic IDs.
+**
+*******************************************************************************
+*/
+
+#define	IDH_CONTENTS	1000
+#define	IDH_CACHEMENU	1001
+#define	IDH_EDITMENU	1002
+#define	IDH_VIEWMENU	1003
+#define	IDH_OPTIONSMENU	1004
+#define	IDH_HELPMENU	1005
+#define	IDH_SELPROFDLG	1006
+#define	IDH_PROFILESDLG	1007
+#define	IDH_EDITPROFDLG	1008
+#define	IDH_PREFSDLG	1009
+#define	IDH_UTCFGDLG	1010
