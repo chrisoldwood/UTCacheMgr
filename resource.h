10d872ce8849440d98a2ad162abde6eaae50abab
Administrator <administrator@chrisoldwood.com>
2004-01-07 22:00:19 +0000

    Modfied Tools | Install hint.

--- resource.h
+++ resource.h
@@ -115,7 +115,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         168
 #define _APS_NEXT_CONTROL_VALUE         1099
 #define _APS_NEXT_SYMED_VALUE           104
